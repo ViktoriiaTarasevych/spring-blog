700323ec55518492913e3427922ec76a842bc85d
ViktoriiaTarasevych <vikka1836@gmail.com>
2023-05-23 20:15:03 +0300

    Add home.html, HomeController

--- src/main/java/com/example/springblog/controller/HomeController.java
+++ src/main/java/com/example/springblog/controller/HomeController.java
@@ -1,5 +1,11 @@
 package com.example.springblog.controller;
 
+
+import com.example.springblog.model.Post;
+import com.example.springblog.service.PostService;
+import com.example.springblog.service.util.Pager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,11 +14,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "home";
+    private final PostService postService;
+
+    @Autowired
+    public HomeController(PostService postService) {
+        this.postService = postService;
     }
 
+    @GetMapping("/home")
+    public String home(@RequestParam(defaultValue = "0") int page,
+                       Model model) {
+
+        Page<Post> posts = postService.findAllOrderedByDatePageable(page);
+        Pager pager = new Pager(posts);
+
+        model.addAttribute("pager", pager);
+
+        return "/home";
+    }
 }
 
